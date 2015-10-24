ed15c815f8b5c398af840f86027a611e874954b2
Bodil Stokke <bodil@bodil.org>
2015-10-25 00:43:08 +0100

    Regenerator test which doesn't break on Node.

--- src/gentest.js
+++ src/gentest.js
@@ -3,9 +3,21 @@ const fnToStr = Function.prototype.toString;
 const isFnRegex = /^\s*function\*/;
 
 function isGen(fn) {
-  if (window.regeneratorRuntime) {
-    /* global regeneratorRuntime */
-    return regeneratorRuntime.isGeneratorFunction(fn);
+  let test = null;
+  try {
+    if (window.regeneratorRuntime) {
+      /* global regeneratorRuntime */
+      test = window.regeneratorRuntime.isGeneratorFunction;
+    }
+  } catch(e) {}
+  try {
+    if (global.regeneratorRuntime) {
+      /* global regeneratorRuntime */
+      test = global.regeneratorRuntime.isGeneratorFunction;
+    }
+  } catch(e) {}
+  if (test !== null) {
+    return test(fn);
   }
   if (typeof fn !== 'function') { return false; }
   var fnStr = toStr.call(fn);
