90b28ee789de27c46fddd0ad114fbc61ad1483e1
Jonathan Gill <mrjonathangill@gmail.com>
2019-09-14 00:57:59 +0200

    Removed compiled files

--- dom.d.ts
+++
@@ -1,3 +0,0 @@
-import * as erx from "./index";
-export declare function now(): number;
-export declare function animationFrame(): erx.Stream<number>;
--- gentest.d.ts
+++
@@ -1,1 +0,0 @@
-export default function isGen(fn: any): fn is GeneratorFunction;
